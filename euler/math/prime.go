f44b09d340ab14bb1d86c7f99e97383b61b9b088
Craig Andrews <craig@lazyrobot.org.uk>
2016-07-01 10:15:43 +0100

    Eratosthenes with a buffer is faster

--- euler/math/prime.go
+++ euler/math/prime.go
@@ -1,17 +1,13 @@
 package math
 
-import (
-	"math"
-)
-
 func PrimeFactors(n int) []int {
 	factors := make([]int, 2)
 	if n == 2 || n == 3 {
 		factors = append(factors, n)
 		return factors
 	}
-	for i := 2; i*i < n; i++ {
-		for n%i == 0 {
+	for i := 2; i * i < n; i++ {
+		for n % i == 0 {
 			n = n / i
 			factors = append(factors, i)
 		}
@@ -23,20 +19,17 @@ func PrimeFactors(n int) []int {
 }
 
 func Primes(n int) <-chan int {
-	ch := make(chan int)
+	ch := make(chan int, n / 10)
 	go func() {
 		marked := make([]bool, n)
-		sqrtn := int(math.Sqrt(float64(n)))
-		for x := 3; x <= sqrtn; x += 2 {
-			if !marked[x] {
-				for y := x * x; y < n; y += x {
-					marked[y] = true
-				}
+		ch <- 2
+		for x := 3; x < n; x += 2 {
+			if marked[x] {
+				continue
 			}
-		}
-		for x := 1; x < n; x += 2 {
-			if !marked[x] {
-				ch <- x
+			ch <- x
+			for y := x; y < n; y += x {
+				marked[y] = true
 			}
 		}
 		close(ch)
